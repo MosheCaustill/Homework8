dd142c43949fe11fa568e25d2c97e30b049a91f0
MosheCaustill <111441912+MosheCaustill@users.noreply.github.com>
2022-10-09 11:09:52 +0300

    script watch message update

--- script.js
+++ script.js
@@ -2,47 +2,53 @@
 //user object//
 function User(username) {
   (this.username = username),
-    (this.follower = []), //מי שעוקב אחריי//
+    (this.followers = []), //מי שעוקב אחריי//
     (this.addFollower = function (newFollower) {
-      this.follower.push(newFollower);
+      this.followers.push(newFollower);
       newFollower.following.push(this);
     });
   (this.following = []), //מי שאני עוקב אחריו//
     (this.addFollowing = function (newFollowing) {
       this.following.push(newFollowing);
-      newFollowing.follower.push(this);
+      newFollowing.followers.push(this);
     });
   this.inbox = []; //פותח תיבת הודעות//
   this.sendMessage = function (message) {
-    this.follower.forEach((followerUser) => {
-      followerUser.inbox.push(this.username + " says: " + message);
+    this.followers.forEach((follower) => {
+      follower.inbox.push(this.username + " says:\nhey "+ follower.username + ' ' + message);
     });
   };
   this.watchMessage = function () {
-    this.inbox.forEach((message) => {
-      console.log(message);
-    });
-    this.inbox = []; //מרוקן הודעות לאחר בצפייה//
+    console.log('\x1b[34m\x1b[4m'+ this.username + "'s InBox:");
+    if (this.inbox.length == 0) {
+      console.log('No New Messages');
+    } else {
+      this.inbox.forEach((message) => {
+        console.log(message);
+      });
+      this.inbox = []; //מרוקן הודעות לאחר הצפייה//
+    }
   };
 }
 
-let user1 = new User("moshe");
-let user2 = new User("osnat");
-let user3 = new User("david");
-let user4 = new User("yakov");
+let user1 = new User("Moshe");
+let user2 = new User("Osnat");
+let user3 = new User("Roi");
+let user4 = new User("Tamar");
 
-user1.addFollower(user2);
-user1.addFollower(user3);
-user3.addFollowing(user4);
+user1.addFollower(user2);//הוספת עוקב//
+user1.addFollower(user3);//הוספת עוקב//
+user3.addFollowing(user4);//הוספת נעקב//
 console.log(user1);
 console.log(user2);
 console.log(user3);
 console.log(user4);
 user1.sendMessage("happy sokot");
 user1.sendMessage("hello world");
-console.log(user2.inbox);
-user1.sendMessage("ma kore olam blad");
-user3.sendMessage("shalom olam ma shlom kolam");
+user1.sendMessage("ma kore olam");
+user4.sendMessage("boker tov olam ma shlom kolam");
 user2.watchMessage();
-console.log(user2.inbox);
-console.log(user3.inbox);
+user1.watchMessage();
+user2.watchMessage();
+user3.watchMessage();
+user4.watchMessage();
